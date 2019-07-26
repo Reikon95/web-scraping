47793ec6b02d6f7a1f4142bc2f180d57142116d3
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-26 14:58:57 +0100

    Update basics.py

--- basics.py
+++ basics.py
@@ -5,5 +5,12 @@ webpage_response = requests.get ('http://nhl.com')
 print(webpage_response.text)
 
   '''
-  The above scrapes the HTML from NHL's website.
+  The above scrapes the src from NHL's website.
+  '''
+
+webpage = webpage_response.content
+
+print(webpage)
+  '''
+  This builds on the above, delivering the content from the page
   '''
